4ee36ee879d7551a702f0a7cfa2a99b00dad9858
xlRexpect <dragosflorea02@yahoo.com>
2023-12-18 19:38:00 +0200

    Mesaj

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class MainApp {
+import java.util.Scanner;
+
+public class MainApp
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("lungime=");
+        int a = scanner.nextInt();
+        System.out.print("latime=");
+        int b = scanner.nextInt();
+
+        System.out.println("aria: "+a*b);
+        System.out.println("perimetrul: "+2*(a+b));
+    }
 }
